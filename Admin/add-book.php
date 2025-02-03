f0bc208b0229b84d0bae41d1c1fd2cf537be9b0b
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2025-02-04 01:10:44 +0800

    Edit Admin UI

---
+++ Admin/add-book.php
@@ -0,0 +1,47 @@
+
+<?php include '../admin/inc/header.php'; ?>
+
+            <!-- Main Content -->
+            <div id="content">
+
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Add Book</h1>
+                    <p class="mb-4">Maintenance</p>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <div class="col-xl-8 col-lg-7">
+
+
+
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php include '../admin/inc/footer.php'; ?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
--- Admin/inc/footer.php
+++ Admin/inc/footer.php
@@ -1,9 +1,9 @@
-   <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; NBS College 2024</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
