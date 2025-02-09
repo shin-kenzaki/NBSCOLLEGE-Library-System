258d7f57ea216187d996f514eed7b8e01ff21684
cayceevans213 <cevangelista2021@student.nbscollege.edu.ph>
2025-02-09 16:12:03 +0800

    Add profile and edit profile
    Add profile and edit profile

--- Admin/inc/header.php
+++ Admin/inc/header.php
@@ -359,7 +359,7 @@
                                 <!-- Dropdown - User Information -->
                                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                     aria-labelledby="userDropdown">
-                                    <a class="dropdown-item" href="#">
+                                    <a class="dropdown-item" href="profile.php">
                                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                         Profile
                                     </a>
@@ -443,6 +443,7 @@
     <script src="js/demo/chart-bar-demo.js"></script>
 
     <script src="inc/assets/DataTables/datatables.min.js"></script>
+
 
 
 
